f7a37ccde945992f2702bb83556e482d6e42b37d
MariamAdly8 <mariamadly633@gmail.com>
2025-01-21 23:00:06 +0200

    recipe

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,5 @@ import 'package:flutter/material.dart';
 import 'package:sizzle_share/RecipeScreen.dart';
 
 void main() {
-  runApp(MaterialApp(
-    debugShowCheckedModeBanner: false,
-    home: RecipeScreen(),
-  ));
+  runApp(RecipeScreen());
 }
