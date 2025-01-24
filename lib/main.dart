8d06d7b61f5c8cb248887e568892bd6cfc9ce425
MariamAdly8 <115795419+MariamAdly8@users.noreply.github.com>
2025-01-24 15:27:52 +0200

    Update main.

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:sizzle_share/LoginPage.dart';
 import 'package:sizzle_share/RecipeScreen.dart';
-import 'package:sizzle_share/SignUpPage.dart';
 import 'package:sizzle_share/homePage.dart';
-import 'package:sizzle_share/LoginPage.dart';
 
 void main() {
-  runApp(LoginPage());
+  runApp(HomePage());
 }
