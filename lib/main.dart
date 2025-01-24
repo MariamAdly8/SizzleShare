2284360467355f63f0e24acb24932faf4df15305
MariamAshraf62 <maraimashraf907@gmail.com>
2025-01-24 02:23:27 +0200

    Signup and Login

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:sizzle_share/LoginPage.dart';
 import 'package:sizzle_share/RecipeScreen.dart';
+import 'package:sizzle_share/SignUpPage.dart';
 import 'package:sizzle_share/homePage.dart';
+import 'package:sizzle_share/LoginPage.dart';
 
 void main() {
-  runApp(RecipeScreen());
+  runApp(LoginPage());
 }
