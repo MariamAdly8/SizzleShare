9610faa593d3dfc37b29b512615943aaa22af5cd
MariamAdly8 <115795419+MariamAdly8@users.noreply.github.com>
2025-01-27 13:54:48 +0200

    :) (#6)

--- lib/RecipeScreen.dart
+++ lib/RecipeScreen.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:sizzle_share/HomePage.dart';
 
 //widget for all section titles
 class SectionTitle extends StatelessWidget {
@@ -30,172 +31,176 @@ class _RecipeScreenState extends State<RecipeScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-        body: SingleChildScrollView(
-          child: Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.center,
-                  children: [
-                    //arrow back
-                    IconButton(
-                      icon: const Icon(Icons.arrow_back),
-                      color: const Color.fromRGBO(253, 93, 105, 1),
-                      onPressed: () {},
-                    ),
-
-                    // title of the screen
-                    Expanded(
-                      child: Center(
-                        child: SectionTitle(txt: "Recipe Details")
-                      ),
-                    ),
-
-                    // favorite button
-                    Container(
-                      height: 35,
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        color: Colors.pink.shade100,
-                      ),
-                      child: IconButton(
-                        icon: Icon(
-                          isFavorite ? Icons.favorite : Icons.favorite_border,
-                          color: const Color.fromRGBO(253, 93, 105, 1),
-                        ),
-                        onPressed: () {
-                          setState(() {
-                            isFavorite = !isFavorite;
-                          });
-                        },
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(height: 10),
-                //recipe name and rate
-                Container(
-                  padding: const EdgeInsets.symmetric(horizontal: 10.0),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(8),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                crossAxisAlignment: CrossAxisAlignment.center,
+                children: [
+                  //arrow back
+                  IconButton(
+                    icon: const Icon(Icons.arrow_back),
                     color: const Color.fromRGBO(253, 93, 105, 1),
-                  ),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: const Text(
-                          'Salami And Cheese Pizza',
-                          style: TextStyle(
-                            fontSize: 22,
-                            color: Colors.white,
-                          ),
-                          softWrap: true,
+                    onPressed: () {
+                      Navigator.push(
+                        context,
+                        MaterialPageRoute(
+                          builder: (context) => const HomePage(),
                         ),
+                      );
+                    },
+                  ),
+
+                  // title of the screen
+                  Expanded(
+                    child: Center(child: SectionTitle(txt: "Recipe Details")),
+                  ),
+
+                  // favorite button
+                  Container(
+                    height: 35,
+                    decoration: BoxDecoration(
+                      shape: BoxShape.circle,
+                      color: Colors.pink.shade100,
+                    ),
+                    child: IconButton(
+                      icon: Icon(
+                        isFavorite ? Icons.favorite : Icons.favorite_border,
+                        color: const Color.fromRGBO(253, 93, 105, 1),
                       ),
-                      const Icon(Icons.star, color: Colors.white, size: 30),
-                      const SizedBox(width: 4),
-                      const Text(
-                        '5',
+                      onPressed: () {
+                        setState(() {
+                          isFavorite = !isFavorite;
+                        });
+                      },
+                    ),
+                  ),
+                ],
+              ),
+              const SizedBox(height: 10),
+              //recipe name and rate
+              Container(
+                padding: const EdgeInsets.symmetric(horizontal: 10.0),
+                decoration: BoxDecoration(
+                  borderRadius: BorderRadius.circular(8),
+                  color: const Color.fromRGBO(253, 93, 105, 1),
+                ),
+                child: Row(
+                  children: [
+                    Expanded(
+                      child: const Text(
+                        'Salami And Cheese Pizza',
                         style: TextStyle(
-                          fontSize: 18,
+                          fontSize: 22,
                           color: Colors.white,
                         ),
+                        softWrap: true,
                       ),
-                    ],
-                  ),
-                ),
-
-                const SizedBox(height: 20),
-                // Details Section
-                Row(
-                  children: [
-                    SectionTitle(txt: "Description"),
-                    SizedBox(width: 8),
-                    Icon(
-                      Icons.alarm,
-                      size: 20,
                     ),
-                    SizedBox(width: 4),
-                    Text(
-                      '45 minutes',
-                      style: TextStyle(fontSize: 14),
-                      softWrap: true,
+                    const Icon(Icons.star, color: Colors.white, size: 30),
+                    const SizedBox(width: 4),
+                    const Text(
+                      '5',
+                      style: TextStyle(
+                        fontSize: 18,
+                        color: Colors.white,
+                      ),
                     ),
                   ],
                 ),
-                const SizedBox(height: 8),
-                const Text(
-                  'This is a quick overview of the ingredients you’ll need for this Salami Pizza recipe. Specific measurements and full recipe instructions are in the printable recipe card below.',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 16),
-                //cuisine section
-                SectionTitle(txt: "Cuisine"),
-                const SizedBox(height: 8),
-                const Text(
-                  'Egyptian',
-                  style: TextStyle(fontSize: 16),
-                ),
-                const SizedBox(height: 16),
+              ),
 
-                // Ingredients Section
-                SectionTitle(txt: "Ingredients"),
-                const SizedBox(height: 8),
-                Column(
+              const SizedBox(height: 20),
+              // Details Section
+              Row(
+                children: [
+                  SectionTitle(txt: "Description"),
+                  SizedBox(width: 8),
+                  Icon(
+                    Icons.alarm,
+                    size: 20,
+                  ),
+                  SizedBox(width: 4),
+                  Text(
+                    '45 minutes',
+                    style: TextStyle(fontSize: 14),
+                    softWrap: true,
+                  ),
+                ],
+              ),
+              const SizedBox(height: 8),
+              const Text(
+                'This is a quick overview of the ingredients you’ll need for this Salami Pizza recipe. Specific measurements and full recipe instructions are in the printable recipe card below.',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 16),
+              //cuisine section
+              SectionTitle(txt: "Cuisine"),
+              const SizedBox(height: 8),
+              const Text(
+                'Egyptian',
+                style: TextStyle(fontSize: 16),
+              ),
+              const SizedBox(height: 16),
+
+              // Ingredients Section
+              SectionTitle(txt: "Ingredients"),
+              const SizedBox(height: 8),
+              Column(
+                children: [
+                  IngredientRow(quantity: '1', name: 'pre-made pizza dough'),
+                  IngredientRow(quantity: '1/2 cup', name: 'pizza sauce'),
+                  IngredientRow(
+                      quantity: '1/2 cup', name: 'shredded mozzarella cheese'),
+                  IngredientRow(quantity: '1/2 cup', name: 'sliced salami'),
+                  IngredientRow(
+                      quantity: '1/4 cup',
+                      name: 'sliced black olives (optional)'),
+                  IngredientRow(
+                      quantity: '1/4 cup', name: 'fresh basil leaves'),
+                ],
+              ),
+              const SizedBox(height: 20),
+              // Comments Section
+              SectionTitle(txt: "Comments"),
+              SizedBox(height: 8),
+              Container(
+                decoration: BoxDecoration(
+                  border: Border.all(
+                    color: Color.fromRGBO(253, 93, 105, 1),
+                  ),
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                padding: const EdgeInsets.all(8.0),
+                child: Row(
                   children: [
-                    IngredientRow(quantity: '1', name: 'pre-made pizza dough'),
-                    IngredientRow(quantity: '1/2 cup', name: 'pizza sauce'),
-                    IngredientRow(
-                        quantity: '1/2 cup',
-                        name: 'shredded mozzarella cheese'),
-                    IngredientRow(quantity: '1/2 cup', name: 'sliced salami'),
-                    IngredientRow(
-                        quantity: '1/4 cup',
-                        name: 'sliced black olives (optional)'),
-                    IngredientRow(
-                        quantity: '1/4 cup', name: 'fresh basil leaves'),
+                    Expanded(
+                      child: const TextField(
+                        decoration: InputDecoration(
+                          hintText: 'Add a comment...',
+                          border: InputBorder.none,
+                        ),
+                      ),
+                    ),
+                    IconButton(
+                      icon: const Icon(
+                        Icons.send,
+                        color: Color.fromRGBO(253, 93, 105, 1),
+                      ),
+                      onPressed: () {},
+                    ),
                   ],
                 ),
-                const SizedBox(height: 20),
-                // Comments Section
-                SectionTitle(txt: "Comments"),
-                SizedBox(height: 8),
-                Container(
-                  decoration: BoxDecoration(
-                    border: Border.all(
-                      color: Color.fromRGBO(253, 93, 105, 1),
-                    ),
-                    borderRadius: BorderRadius.circular(8),
-                  ),
-                  padding: const EdgeInsets.all(8.0),
-                  child: Row(
-                    children: [
-                      Expanded(
-                        child: const TextField(
-                          decoration: InputDecoration(
-                            hintText: 'Add a comment...',
-                            border: InputBorder.none,
-                          ),
-                        ),
-                      ),
-                      IconButton(
-                        icon: const Icon(
-                          Icons.send,
-                          color: Color.fromRGBO(253, 93, 105, 1),
-                        ),
-                        onPressed: () {},
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+              ),
+            ],
           ),
         ),
-      );
+      ),
+    );
   }
 }
 
