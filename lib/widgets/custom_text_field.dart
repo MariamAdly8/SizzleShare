d62901fda3bc8d165146baf4fb7f37df989cc5bf
MariamAshraf62 <149336430+MariamAshraf62@users.noreply.github.com>
2025-02-05 14:58:56 +0200

    auth (#9)

--- lib/widgets/custom_text_field.dart
+++ lib/widgets/custom_text_field.dart
@@ -4,17 +4,33 @@ class CustomTextField extends StatelessWidget {
   final String? hintText;
   final Widget? suffixIcon;
   final bool obscureText;
+  final TextEditingController? controller;
+  final VoidCallback? onEditingComplete; // إضافة `onEditingComplete`
+  final Function(String)? onChanged; // إضافة `onChanged` للتحقق أثناء الكتابة
+  final TextInputType? keyboardType; // تحديد نوع الإدخال
+  final String? Function(String?)? validator; // إضافة التحقق باستخدام validator
 
-  const CustomTextField({super.key, 
+  const CustomTextField({
+    super.key,
     this.hintText,
     this.suffixIcon,
     this.obscureText = false,
-  }) ;
+    this.controller,
+    this.onEditingComplete, // استقباله كـ `callback`
+    this.onChanged, // استقباله كـ `callback`
+    this.keyboardType, // إضافة نوع الإدخال
+    this.validator, // إضافة validator
+  });
 
   @override
   Widget build(BuildContext context) {
-    return TextField(
+    return TextFormField(
+      controller: controller,
       obscureText: obscureText,
+      onEditingComplete: onEditingComplete, // تمريره عند الحاجة
+      onChanged: onChanged, // تمريره للتحقق أثناء الكتابة
+      keyboardType: keyboardType, // إضافة نوع الإدخال
+      validator: validator, // التحقق باستخدام validator
       decoration: InputDecoration(
         hintText: hintText,
         hintStyle: TextStyle(
