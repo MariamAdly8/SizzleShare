bcc48d3b38e7b2754e7cc5a1a7b1782d428305fc
74melody <melodymaged@gmail.com>
2025-05-12 15:45:01 +0300

    front-end

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:sizzle_share/HomePage.dart';
 import 'package:sizzle_share/LoginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
